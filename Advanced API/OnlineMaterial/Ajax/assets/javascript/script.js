2b5764e958543a08d6b1a8c0b0858b33d82ef9a9
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-08-13 15:36:28 +0530

    use of ready state and status code with real example

--- Advanced API/OnlineMaterial/Ajax/assets/javascript/script.js
+++ Advanced API/OnlineMaterial/Ajax/assets/javascript/script.js
@@ -8,20 +8,29 @@ document.getElementById("btn").addEventListener("click",function () {
     console.log("Request started")
 
     let http = new XMLHttpRequest();
-    console.log(http.readyState, "After the html Object");
-
+    // console.log(http.readyState, "After the html Object");
+    // console.log(http.status, "After the html Object");//prints the response status code
+    //
     http.open("GET","/Advanced API/OnlineMaterial/Ajax/db/db.txt")
-    console.log(http.readyState, "After the html open");
-
+    console.log(http.status);
+    // console.log(http.readyState, "After the html open");
+    //
     http.send();
-    console.log(http.readyState, "After the html send");
+    console.log(http.status);
 
-    console.log(http.responseText);
-    console.log(http.readyState, "After the html response");
+    // console.log(http.readyState, "After the html send");
+    //
+    // console.log(http.responseText);
+    // console.log(http.readyState, "After the html response");
 
     http.onreadystatechange = function (){
-        console.log(http.readyState, "Inside on ready state");
+        console.log(http.readyState);
+        console.log(http.status);
         console.log(http.response);
+        if (http.readyState === 4 && http.status === 200){
+            console.log(http.response);
+            document.getElementById("desc").innerText = http.responseText;
+        }
 
     }
 

c79c87bcee1f6f0f25fea50692ac251934e33b2c
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-08-15 16:49:49 +0530

    enable syncronus in java script (java script default / blocking)

--- Advanced API/OnlineMaterial/Ajax/assets/javascript/script.js
+++ Advanced API/OnlineMaterial/Ajax/assets/javascript/script.js
@@ -11,29 +11,30 @@ document.getElementById("btn").addEventListener("click",function () {
     // console.log(http.readyState, "After the html Object");
     // console.log(http.status, "After the html Object");//prints the response status code
     //
-    http.open("GET","/Advanced API/OnlineMaterial/Ajax/db/db.txt")
+    http.open("GET","/Advanced API/OnlineMaterial/Ajax/db/db.txt",false);// false = syncronous (blocking)
     console.log(http.status);
     // console.log(http.readyState, "After the html open");
     //
     http.send();
-    console.log(http.status);
+    // console.log(http.status);
 
     // console.log(http.readyState, "After the html send");
     //
     // console.log(http.responseText);
     // console.log(http.readyState, "After the html response");
 
-    http.onreadystatechange = function (){
-        console.log(http.readyState);
-        console.log(http.status);
-        console.log(http.response);
-        if (http.readyState === 4 && http.status === 200){
-            console.log(http.response);
-            document.getElementById("desc").innerText = http.responseText;
-        }
+    // http.onreadystatechange = function (){
+    //     console.log(http.readyState);
+    //     console.log(http.status);
+    //     console.log(http.response);
+    //     if (http.readyState === 4 && http.status === 200){
+    //         console.log(http.response);
+    //         document.getElementById("desc").innerText = http.responseText;
+    //     }
+    //
+    // }
 
-    }
-
+    console.log(http.responseText);
     console.log("Request finished")
 
 })

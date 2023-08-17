6de9bc2fbed0be480fd3ab07b6b375d1ca636f41
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-08-17 22:43:46 +0530

    first response sended

--- Advanced API/OnlineMaterial/JavaEE/01_HelloServlet/src/HelloServlet.java
+++ Advanced API/OnlineMaterial/JavaEE/01_HelloServlet/src/HelloServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "MyServlet",urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
@@ -12,5 +13,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("request ekak labuna");
+        PrintWriter writer = resp.getWriter();
+        writer.write("rensponse send");
     }
 }

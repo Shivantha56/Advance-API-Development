4910b868e061ff8e6168a20a5ef0d7b4f952d615
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-08-21 18:23:39 +0530

    cros policy identified

---
+++ Advanced API/OnlineMaterial/JavaEE/02_HelloServlet/_02_HelloServlet/web2/assets/javascript/script01.js
@@ -0,0 +1,84 @@
+//text file
+$("#button01").click(function () {
+
+    $.ajax({
+        url: "db/db.txt",
+        success: function (resp) {
+            console.log(resp)
+            $("#display01").text(resp);
+        },
+        error: function (err) {
+            console.log(err)
+        },
+        async: true
+    });
+
+});
+
+//XML object
+$("#button02").click(function () {
+    $("#table01>tr").empty();
+    $.ajax({
+        url: "db/db.xml",
+        success: function (resp) {
+            console.log(resp)
+
+            for (let i = 0; i < $(resp).children().children().length; i++) {
+
+                let id = $(resp).children().children().eq(i).find("id").text();
+                let name = $(resp).children().children().eq(i).find("name").text();
+                let address = $(resp).children().children().eq(i).find("address").text();
+
+                $("#table01").append(" <tr>\n" +
+                    `        <td>${id}</td>\n` +
+                    `        <td>${name}</td>\n` +
+                    `        <td>${address}</td>\n` +
+                    "    </tr>");
+
+            }
+        },
+        error: function (err) {
+            console.log(err)
+        },
+        async: true
+    });
+});
+
+//json object
+$("#button03").click(function () {
+
+    $("#table02>tr").empty();
+
+    $.ajax({
+        url: "db/db.json",
+        success: function (resp) {
+            console.log(resp);
+
+            for (const i in resp) {
+                let id = resp[i].id;
+                let name = resp[i].name;
+                let address = resp[i].address;
+
+                $("#table02").append(" <tr>\n" +
+                    `        <td>${id}</td>\n` +
+                    `        <td>${name}</td>\n` +
+                    `        <td>${address}</td>\n` +
+                    "    </tr>");
+
+            }
+
+
+        },
+        error: function (err) {
+            console.log(err)
+        },
+        async: true
+    });
+
+
+});
+
+
+
+
+

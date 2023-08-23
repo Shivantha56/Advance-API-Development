6282b977f7a3a148415f0f55980ce5335a9887fc
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-08-23 14:33:29 +0530

    Initialize project mapping spec

--- Advanced API/OnlineMaterial/JavaEE/02_HelloServlet/_02_HelloServlet/src/MyServlet.java
+++ Advanced API/OnlineMaterial/JavaEE/02_HelloServlet/_02_HelloServlet/src/MyServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -11,7 +12,14 @@ public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("application/html");
         PrintWriter writer = resp.getWriter();
-        writer.write("Application context");
+        writer.write("<h1>Application context</h1>");
+        writer.close();
+        String method = req.getMethod();
+        System.out.println(method);
+
+
     }
 }

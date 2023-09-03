25d815c752120e7f3342f410565b3e222d531f70
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-03 16:26:27 +0530

    connection pool module content updated

--- Advanced API/OnlineMaterial/JavaEE/_06_Json_processing/src/CustomerServlet.java
+++ Advanced API/OnlineMaterial/JavaEE/_06_Json_processing/src/CustomerServlet.java
@@ -5,12 +5,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
 
+    DataSource dataSource;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
--- Advanced API/OnlineMaterial/JavaEE/_07_InBuildCP/src/Servlet/CustomerServlet.java
+++ Advanced API/OnlineMaterial/JavaEE/_07_InBuildCP/src/Servlet/CustomerServlet.java
@@ -1,23 +1,29 @@
 package Servlet;
 
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
 
-    @Resource("name =java:comp/env/jdbc.pool")
+
+    @Resource(name= "java:comp/env/jdbc.pool")
     DataSource dataSource;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         try {
             Connection connection = dataSource.getConnection();
-            connection.prepareStatement("").executeUpdate();
+            System.out.println(connection);
+//            connection.prepareStatement("").executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }

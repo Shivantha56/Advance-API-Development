5446c880fcbcd5ee4bb86eef0cb43aecddc183b5
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-06 11:38:03 +0530

    scope annotation

--- Advanced API/OnlineMaterial/Spring/AAD-SPRING/03_Core/01_Core/src/main/java/AppInitializer.java
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/03_Core/01_Core/src/main/java/AppInitializer.java
@@ -1,6 +1,8 @@
+import bean.MyConnection;
 import bean.SpringBeanOne;
 import bean.SpringBeanThree;
 import bean.SpringBeanTwo;
+import bean.insidebean.MySubClass01;
 import config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -23,13 +25,29 @@ public class AppInitializer {
         anc.register(AppConfig.class);
         anc.refresh();
 
-        SpringBeanOne springBean01 = anc.getBean(SpringBeanOne.class);
-        SpringBeanTwo springBean02 = anc.getBean(SpringBeanTwo.class);
+//        SpringBeanOne springBean01 = anc.getBean(SpringBeanOne.class);
+//        SpringBeanTwo springBean02 = anc.getBean(SpringBeanTwo.class);
+//        System.out.println(springBean01);
+//        System.out.println(springBean02);
+//
+//        SpringBeanThree springBean03 = anc.getBean(SpringBeanThree.class);
+//        System.out.println(springBean03);
+        Object springBean01 = anc.getBean("springBeanOne"); //get bean from bean id
         System.out.println(springBean01);
+        Object springBean02 = anc.getBean("BeanTwo"); //Bean two is not a default id, it is custom generated id
         System.out.println(springBean02);
+        MyConnection bean = anc.getBean(MyConnection.class);
+        System.out.println("this is the my connection "+bean);
 
-        SpringBeanThree springBean03 = anc.getBean(SpringBeanThree.class);
-        System.out.println(springBean03);
+        MySubClass01 bean1 = anc.getBean(MySubClass01.class);
+        MySubClass01 bean2 = anc.getBean(MySubClass01.class);
+        MySubClass01 bean3 = anc.getBean(MySubClass01.class);
+
+        System.out.println(bean1);
+        System.out.println(bean2);
+        System.out.println(bean3);
+
+
     }
 
 
--- Advanced API/OnlineMaterial/Spring/AAD-SPRING/03_Core/01_Core/src/main/java/config/AppConfig.java
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/03_Core/01_Core/src/main/java/config/AppConfig.java
@@ -1,15 +1,24 @@
 package config;
 
+import bean.MyConnection;
 import bean.SpringBeanOne;
 import beans.Demo02;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+//Bean definition source
 @Configuration
-@ComponentScan(basePackageClasses = {Demo02.class, SpringBeanOne.class})
+//@ComponentScan(basePackageClasses = {Demo02.class, SpringBeanOne.class})
+@ComponentScan(basePackages = "bean")
 public class AppConfig {
     public AppConfig(){
         System.out.println("This is the configuration class");
+    }
+
+    @Bean("myBean")
+    public MyConnection getConnection(){
+        return new MyConnection();
     }
 
 }

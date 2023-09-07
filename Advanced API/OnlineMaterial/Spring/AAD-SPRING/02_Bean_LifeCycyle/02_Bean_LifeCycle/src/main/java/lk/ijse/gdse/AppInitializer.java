3c7018b753934fde9e4b76059ac3691fe620c13d
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-07 15:40:39 +0530

    content updated

--- Advanced API/OnlineMaterial/Spring/AAD-SPRING/02_Bean_LifeCycyle/02_Bean_LifeCycle/src/main/java/lk/ijse/gdse/AppInitializer.java
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/02_Bean_LifeCycyle/02_Bean_LifeCycle/src/main/java/lk/ijse/gdse/AppInitializer.java
@@ -1,4 +1,15 @@
 package lk.ijse.gdse;
 
+import lk.ijse.gdse.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppInitializer {
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.registerShutdownHook();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+    }
 }
---
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/02_Bean_LifeCycyle/02_Bean_LifeCycle/src/main/java/lk/ijse/gdse/bean/MyConnection.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse.bean;
+
+public class MyConnection {
+
+    public MyConnection() {
+        System.out.println("This is the myconnection");
+    }
+}

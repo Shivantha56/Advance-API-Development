9953726cf5b0127d2dbdafec1f91842915ea50d4
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-10 15:44:39 +0530

    di injection with spring using interface

---
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/03_DI_with_Springs/src/main/java/lk/ijse/gdse/di/Di.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.di;
+
+public interface Di {
+
+    void inject(Kamali kamali);
+
+}
--- Advanced API/OnlineMaterial/Spring/AAD-SPRING/03_DI_with_Springs/src/main/java/lk/ijse/gdse/di/Kamal.java
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/03_DI_with_Springs/src/main/java/lk/ijse/gdse/di/Kamal.java
@@ -4,23 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Kamal {
+public class Kamal implements Di {
    // @Autowired // property injection
     SuperKamali kamali;
 
-    @Autowired
-    public Kamal(SuperKamali kamali) {
-        this.kamali = kamali;
-        System.out.println("kamal is initiated");
-    }
+    //constructor throught
+//    @Autowired
+//    public Kamal(SuperKamali kamali) {
+//        this.kamali = kamali;
+//        System.out.println("kamal is initiated");
+//    }
 
-    @Autowired
-    public void setkaamali (SuperKamali kamali){
-        this.kamali = kamali;
-    }
+    // setter method
+//    @Autowired
+//    public void setkaamali (SuperKamali kamali){
+//        this.kamali = kamali;
+//    }
 
     public void chatWithKamali(){
 
        kamali.chat();
+    }
+
+    // Interface through injection
+    @Autowired
+    @Override
+    public void inject(Kamali kamali) {
+        this.kamali = kamali;
     }
 }

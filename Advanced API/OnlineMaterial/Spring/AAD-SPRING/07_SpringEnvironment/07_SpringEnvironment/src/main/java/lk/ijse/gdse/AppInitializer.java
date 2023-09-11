236cdf39feabf4f2194bf876f135a53118913323
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-11 22:38:04 +0530

    how to get system variables

--- Advanced API/OnlineMaterial/Spring/AAD-SPRING/07_SpringEnvironment/07_SpringEnvironment/src/main/java/lk/ijse/gdse/AppInitializer.java
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/07_SpringEnvironment/07_SpringEnvironment/src/main/java/lk/ijse/gdse/AppInitializer.java
@@ -1,11 +1,24 @@
 package lk.ijse.gdse;
 
+import lk.ijse.gdse.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Map;
 
 public class AppInitializer {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.registerShutdownHook();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+
+//        get system variable
+//        System.getenv();
+        Map<String, String> getenv = System.getenv();
+        for (String key: getenv.keySet()) {
+            System.out.println(key+" : "+getenv.get(key));
+        }
 
     }
 }

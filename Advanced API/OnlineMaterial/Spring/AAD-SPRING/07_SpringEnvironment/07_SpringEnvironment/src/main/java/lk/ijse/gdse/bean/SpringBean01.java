9896a99ad08c4c0f4d2116253957df23a9be20a9
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-11 23:26:35 +0530

    get environment variable value using spring value annotation from bundle source

--- Advanced API/OnlineMaterial/Spring/AAD-SPRING/07_SpringEnvironment/07_SpringEnvironment/src/main/java/lk/ijse/gdse/bean/SpringBean01.java
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/07_SpringEnvironment/07_SpringEnvironment/src/main/java/lk/ijse/gdse/bean/SpringBean01.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean01 implements InitializingBean {
+    //property placeholder
     @Value("${user.name}")
     private String environmetn;
+
+    @Value("${ijse.application.name}")
+    private String projectName;
 
     public SpringBean01() {
         System.out.println("This is the spring bean one initiated");
@@ -16,5 +20,6 @@ public class SpringBean01 implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println(environmetn);
+        System.out.println(projectName);
     }
 }

90eb88e8d80268f7a43dd97ad35730cc0867299b
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-11 22:43:32 +0530

    how to get java environmental variables

--- Advanced API/OnlineMaterial/Spring/AAD-SPRING/07_SpringEnvironment/07_SpringEnvironment/src/main/java/lk/ijse/gdse/AppInitializer.java
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/07_SpringEnvironment/07_SpringEnvironment/src/main/java/lk/ijse/gdse/AppInitializer.java
@@ -4,6 +4,7 @@ import lk.ijse.gdse.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Map;
+import java.util.Properties;
 
 public class AppInitializer {
 
@@ -20,5 +21,12 @@ public class AppInitializer {
             System.out.println(key+" : "+getenv.get(key));
         }
 
+        //get java environmental variable
+//        System.getProperty()
+
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()){
+            System.out.println(key+" : "+properties.get(key));
+        }
     }
 }

df9bf9c1ee34218355535d9e0339db2c60b9a24f
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-12 23:00:23 +0530

    web initializer implement

--- Advanced API/OnlineMaterial/Spring/AAD-SPRING/08_SpringWebMVC/_08_SpringWebMVC/src/main/java/lk/ijse/gdse/config/WebAppConfig.java
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/08_SpringWebMVC/_08_SpringWebMVC/src/main/java/lk/ijse/gdse/config/WebAppConfig.java
@@ -1,4 +1,12 @@
 package lk.ijse.gdse.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.gdse.bean")
+@EnableWebMvc
 public class WebAppConfig {
 }

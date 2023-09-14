2a67cd9893031a787a17002d42a49abb5cbe3844
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2023-09-14 19:10:38 +0530

    content update

---
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/12_SpringDataJPA/SpringDataJPA/src/main/java/lk/ijse/gdse/config/WebAppConfig.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.gdse")
+public class WebAppConfig {
+
+
+}
---
+++ Advanced API/OnlineMaterial/Spring/AAD-SPRING/12_SpringDataJPA/SpringDataJPA/src/main/java/lk/ijse/gdse/config/WebRootConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class WebRootConfig {
+}
